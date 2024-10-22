32132a7159e3af33b5b373b561a09833475f2830
Rupesh Rao <rupeshrao1709@gmail.com>
2024-10-22 23:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -355,4 +355,4 @@ def chatbot():
 if __name__ == '__main__':
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    app.run(debug=True)
+        app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
